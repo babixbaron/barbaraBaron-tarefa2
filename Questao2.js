9776cf70cf01125121f5d81d55bd37ce7f5a4aa5
Bárbara Baron <bcostabaron@gmail.com>
2022-04-03 18:17:01 -0300

    Segunda questão

---
+++ Questao2.js
@@ -0,0 +1,10 @@
+// Calcule o tempo de uma viagem de carro.
+// Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distancia = 700
+
+let velocidade = 100
+
+let tempoDeViagem = distancia/velocidade
+
+console.log('A viagem levará em média ' + tempoDeViagem + ' horas.')
