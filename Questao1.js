bb1bdd571159665c458cf77cf854c2c64bf56ce5
Bárbara Baron <bcostabaron@gmail.com>
2022-04-03 18:13:21 -0300

    Primeira questão

---
+++ Questao1.js
@@ -0,0 +1,10 @@
+//Solicite o preço de uma mercadoria e o percentual de desconto 20%. 
+// Exiba no console o valor do desconto e o preço a pagar.
+
+let preçoMercadoria = 50
+
+let desconto = 0.2
+
+let preçoFinal = preçoMercadoria - (preçoMercadoria * desconto)
+
+console.log('O preço a ser pago com 20% de desconto é de R$' + preçoFinal + '.')
