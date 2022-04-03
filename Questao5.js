b88cbdb862d4da9bb68aabceee28fd01ab6b349f
Bárbara Baron <bcostabaron@gmail.com>
2022-04-03 18:34:25 -0300

    Quinta questão

---
+++ Questao5.js
@@ -0,0 +1,11 @@
+// Receba do usuários 3 números e calcule a média entre eles.
+
+let primeiroNumero = 6
+
+let segundoNumero = 10
+
+let terceiroNumero = 8
+
+let media = (primeiroNumero + segundoNumero + terceiroNumero) / 3
+
+console.log('A média entre os números recebidos é: ' + media)
