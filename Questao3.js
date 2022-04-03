b65c7573781c387fa15a2e99f5ac8faac1f55feb
Bárbara Baron <bcostabaron@gmail.com>
2022-04-03 18:20:22 -0300

    Terceira questão

---
+++ Questao3.js
@@ -0,0 +1,10 @@
+// Crie um algoritmo que leia o valor de um jantar, 
+// calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+
+let valorJanta = 200
+
+let taxaGarçom = 0.1
+
+let valorTotal = valorJanta + (valorJanta * taxaGarçom)
+
+console.log('O valor total da janta mais os 10% é de R$' + valorTotal + '.')
