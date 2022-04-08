209b839b58907c4bf8004e94f76b1e25a3df0fce
Bárbara Baron <bcostabaron@gmail.com>
2022-04-07 21:29:52 -0300

    Terceira questão

--- Questao3.js
+++ Questao3.js
@@ -5,6 +5,8 @@ let valorJanta = 200
 
 let taxaGarçom = 0.1
 
-let valorTotal = valorJanta + (valorJanta * taxaGarçom)
+let valorJantaMaisTaxa = valorJanta * taxaGarçom
 
-console.log('O valor total da janta mais os 10% é de R$' + valorTotal + '.')
+let valorTotal = valorJanta + valorJantaMaisTaxa
+
+console.log('O valor total da janta com os 10% é de R$' + valorTotal + '.')
